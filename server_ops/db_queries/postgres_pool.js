d00bf5d2e1dd7e00007e6d348e91222a7bad9e26
Alex Koppel <75754097+Ezayji@users.noreply.github.com>
2021-03-21 22:14:44 +0200

    Trying to resolve postgres connection problem

--- server_ops/db_queries/postgres_pool.js
+++ server_ops/db_queries/postgres_pool.js
@@ -7,7 +7,7 @@ const Pool = require('pg').Pool;
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PW}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`;
 
-const prodConfig = process.env.DATABASE_URL;
+const prodConfig = process.env.DATABASE_URL + '?ssl=true';
 
 const pool = new Pool({
     connectionString: process.env.NODE_ENV === 'production' ? prodConfig : devConfig
